44e4f5e411351e9c785bbfe721c375d1f66f16cb
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-05-27 14:42:47 +0200

    Update CSS and toggles

--- src/lib/TopBar/Right.svelte
+++ src/lib/TopBar/Right.svelte
@@ -5,11 +5,16 @@
     KeywordPaneVisible,
     VariablePaneVisible,
   } from "../../ts/ui/main";
+  import { PublicVariableStore } from "../../ts/var/main";
   import Toggle from "./Right/Toggle.svelte";
 </script>
 
 <div class="right">
-  <Toggle store={VariablePaneVisible} caption="Variables">view_in_ar</Toggle>
+  <Toggle
+    store={VariablePaneVisible}
+    caption="Variables"
+    disabled={!Object.entries($PublicVariableStore).length}>view_in_ar</Toggle
+  >
   <Toggle store={CanvasPaneVisible} caption="Show Canvas">visibility</Toggle>
   <Toggle store={KeywordPaneVisible} caption="Keywords">account_tree</Toggle>
 </div>
--- src/lib/TopBar/Right/Toggle.svelte
+++ src/lib/TopBar/Right/Toggle.svelte
@@ -3,9 +3,14 @@
 
   export let store: Writable<boolean>;
   export let caption = "";
+  export let disabled = false;
 
   function toggle() {
     $store = !$store;
+  }
+
+  $: {
+    if (disabled) $store = false;
   }
 </script>
 
@@ -14,6 +19,7 @@
   class:active={$store}
   on:click={toggle}
   title={caption}
+  {disabled}
 >
   <slot />
 </button>
